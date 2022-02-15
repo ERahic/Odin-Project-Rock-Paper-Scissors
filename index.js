327f06fae5c2eb5ac10569befb6d8c417f9c18a0
Edis Rahic <rahic.edis@gmail.com>
2022-02-14 23:57:12 -0500

    Version 1.2
    Improved user interference and included a rematch button whoever wins 5 rounds first.

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ let playerSelection = ''
 let playerScore = 0
 let computerScore = 0
 
-const playerScoreAdd = document.querySelector('.player-points');
-const computerScoreAdd = document.querySelector('.computer-points');
+let playerScoreAdd = document.querySelector('.player-points');
+let computerScoreAdd = document.querySelector('.computer-points');
 
 /* RESULTS FOR THE ROUND */
-let result = ''
+const gameEnd = document.querySelector('.endGame');
 const endResult = document.querySelector('.result');
-
+let result = ''
 
 
     /* Function that allows the computer to randomly choose either rock, paper or scissors */
@@ -55,18 +55,42 @@ scissorsBTN.addEventListener("click", (e) => {
 and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock" */
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        endResult.innerHTML = "It's a tie!";
+        endResult.textContent = "It's a tie!";
     } else if ((playerSelection === "ROCK" && computerSelection === "PAPER")
         || (playerSelection === "PAPER" && computerSelection === "SCISSORS")
         || (playerSelection === "SCISSORS" && computerSelection === "ROCK")) {
-            endResult.innerHTML = "You Lose! " + computerSelection + " beats " + playerSelection
-            computerScoreAdd.innerHTML++
+            endResult.textContent = "You Lose! " + computerSelection + " beats " + playerSelection;
+            computerScoreAdd.innerHTML++;
+            computerScore++;
+            console.log("computer score " + computerScore);
         } else {
-            endResult.innerHTML = "You Win! " + playerSelection + " beats " + computerSelection
-            playerScoreAdd.innerHTML++
-        }
+            endResult.textContent = "You Win! " + playerSelection + " beats " + computerSelection;
+            playerScoreAdd.innerHTML++;
+            playerScore++;
+            console.log("player score " + playerScore);
+        } 
+        game();
 }
 
 /* Make your function's playerSelection parameter case-insensitive (so users can input rock, ROCK, or RocK or any other variation) */
 /* You want to return the result of this funciton call, not console.log() them.  */
 /* Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end. */
+
+function game() {
+    if (playerScore === 5) {
+        gameEnd.textContent = 'Congrats! You won the game! Please press the button below for a rematch.'
+        console.log('Working!')
+        gameReset();
+    } else if (computerScore === 5) {
+        gameEnd.textContent = 'Better luck next time. You lost the game! Please press the button below for a rematch.'
+        console.log('Working!')
+        gameReset();
+    }
+}
+
+function gameReset() {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreAdd = 0;
+    computerScoreAdd = 0;
+}
